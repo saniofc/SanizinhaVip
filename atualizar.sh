ddbba8effe3b8e4a0e68b311b34ee11717f00a56
saniofc <sanibronx21@gmail.com>
2025-07-17 23:12:33 -0300

    Update atualizar.sh

--- atualizar.sh
+++ atualizar.sh
@@ -3,25 +3,19 @@
 GREEN='\033[1;32m'
 BLUE='\033[1;34m'
 NC='\033[0m'
-
 clear
 printf "${BLUE}==========================================\n"
 printf "${GREEN}     ⌛ Atualizando bot Sanizinha...     \n"
 printf "${BLUE}==========================================${NC}\n"
 sleep 1
-
 # Adiciona exceção para o diretório
 git config --global --add safe.directory /storage/emulated/0/SanizinhaBot
-
 # Vai para a pasta do bot
 cd /storage/emulated/0/SanizinhaBot || exit
-
 # Atualiza o repositório com rebase para evitar conflito de branches
 git pull --rebase origin main
-
 # Mensagem final
 printf "${GREEN}✅ BOT ATUALIZADO COM SUCESSO! 💕😻✨${NC}\n"
 printf "${BLUE}🚀 Iniciando o bot...${NC}\n"
-
 # Inicia o bot
 sh sani.sh
